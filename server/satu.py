0355a557925d562a9051be000b8fdb474109a5c9
sirius <sirius.ustc@gmail.com>
2018-07-05 20:57:26 +0800

    use asyncio

--- server/satu.py
+++ server/satu.py
@@ -1,105 +1,44 @@
-#!/usr/bin/env python3
+#/usr/bin/env python3
 
+import asyncio
 import sys
-import socket
 import json
-import datetime as dt
-import multiprocessing as mp
-import tkinter as tk
-from tkinter import messagebox
+import multiprocess as mp
 
+class ServerProtocol:
+    def connection_made(self, transport):
+        self.transport = transport
 
-class ClientManager:
-    def __init__(self, config):
-        self.format = 'stream'
-        self.clients = {}
-        self.config = config
-
-    def get(self, addr):
-        self.format = self.ename(addr)
-        if addr not in self.clients:
-            client = Client(addr, self.format)
-            self.clients[addr] = client
-        return self.clients[addr]
-
-    def close(self):
-        for client in self.clients:
-            client.close()
-        self.clients = {}
-
-    def ename(self, addr):
-        # format the filename by format string in json
-        name = ''
-        _format = self.config['format']
-        parts = _format.split("%")
-        name += parts[0]
-        for i in range(1,len(parts)):
-            if parts[i][0] == '@': # address
-                name += addr[0]
-            elif parts[i][0] == '#': # port number
-                name += str(addr[1])
-            else:
-                name += dt.datetime.today().strftime('%'+parts[i][0])
-            name += parts[i][1:]
-        return name
-
-
-class Client:
-    def __init__(self, addr, format):
-        self.addr = addr
-        self.format = format
-        self.stream_file = None
-
-    def get_stream_file(self):
-        if self.stream_file:
-            return self.stream_file
-        self.stream_file = open(self.format, 'ab', buffering=0)
-        return self.stream_file
-
-    def store(self, data):
-        self.get_stream_file().write(data)
-
-    def close(self):
-        self.stream_file.close()
-
+    def datagram_received(self, data, addr):
+        message = data.decode()
+        ret = bytes(chr(len(message)%128)+'\n', "ascii")
+        print('Received %r from %s' % (data, addr))
+        #self.deal(self.config, data)
+        #self.clients.get(addr).store(data)
+        #self.transport.sendto(ret, addr)
 
 class Server:
     def __init__(self, config):
-        self.clients = ClientManager(config)
         self.config = config
-        self.udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udpsock.bind((config['addr'], config['port']))
+        #self.deal = deal
+        self.loop = asyncio.get_event_loop()
+        self.listen = self.loop.create_datagram_endpoint(ServerProtocol, local_addr=(self.config['addr'], self.config['port']))
+        self.transport, self.protocol = self.loop.run_until_complete(self.listen)
 
     def __enter__(self):
+        print("Starting UDP server")
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.clients.close()
+        self.transport.close()
+        self.loop.close()
+    # One protocol instance will be created to serve all client requests
 
     def run(self):
         try:
-            while True:
-                data, addr = self.udpsock.recvfrom(2048)
-                #self.udpsock.sendto(b'test', addr)
-                self.udpsock.sendto(bytes(chr(len(data)%128)+'\n', "ascii"), addr)
-                print('received: ', data, 'from', addr)
-                self.deal(data)
-                self.clients.get(addr).store(data)
+            self.loop.run_forever()
         except KeyboardInterrupt:
-            exit(0)
-    
-    def deal(self, data):
-        """the function user use to deal with data"""
-        if 'up' in self.config:
-            try:
-                if int(data) >= self.config['up']:
-                    root = tk.Tk()
-                    root.withdraw()
-                    messagebox.showinfo('warning', 'temperature too high!')
-                    print("warning! temperature too high!")
-            except ValueError:
-                print("please enter a number!")
-
+            pass
 
 def main(args):
     config = []
